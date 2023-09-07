06b925164a8384cb2753fb4a891b9280024ba429
Documenter.jl <documenter@juliadocs.github.io>
2023-09-07 19:48:57 +0000

    build based on 662925a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NeuralQuantumStates","category":"page"},{"location":"#NeuralQuantumStates","page":"Home","title":"NeuralQuantumStates","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NeuralQuantumStates.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NeuralQuantumStates]","category":"page"}]
+}
